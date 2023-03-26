715b944bdff331200465f190b4ec746a20f2073b
Arman <mesropyanarman4@gmail.com>
2023-03-26 12:29:13 +0400

    files upload

--- src/pages/skills/common/skills.js
+++ src/pages/skills/common/skills.js
@@ -12,7 +12,7 @@ export let mySkills = [
         id: Math.random().toString(36).substr(2, 9),
         img: 'icon-css3-plain-wordmark',
         title: 'CSS',
-        stars: 4.5,
+        stars: 5,
         starFull: "icon-star-full",
         starHalf: "icon-star-half",
         starEmpty: 'icon-star-empty'
@@ -30,7 +30,7 @@ export let mySkills = [
         id: Math.random().toString(36).substr(2, 9),
         img: 'icon-less-plain-wordmark',
         title: 'LESS',
-        stars: 4.5,
+        stars: 5,
         starFull: "icon-star-full",
         starHalf: "icon-star-half",
         starEmpty: 'icon-star-empty'
@@ -39,7 +39,7 @@ export let mySkills = [
         id: Math.random().toString(36).substr(2, 9),
         img: 'icon-javascript-plain',
         title: 'Javascript',
-        stars: 4,
+        stars: 5,
         starFull: "icon-star-full",
         starHalf: "icon-star-half",
         starEmpty: 'icon-star-empty'
@@ -48,7 +48,7 @@ export let mySkills = [
         id: Math.random().toString(36).substr(2, 9),
         img: 'icon-jquery-original',
         title: 'Jquery',
-        stars: 4,
+        stars: 4.5,
         starFull: "icon-star-full",
         starHalf: "icon-star-half",
         starEmpty: 'icon-star-empty'
@@ -74,8 +74,8 @@ export let mySkills = [
     {
         id: Math.random().toString(36).substr(2, 9),
         img: 'icon-redux-original',
-        title: 'Redux/React-Redux/Toolkit',
-        stars: 4,
+        title: 'Redux',
+        stars: 4.5,
         starFull: "icon-star-full",
         starHalf: "icon-star-half",
         starEmpty: 'icon-star-empty'
@@ -93,7 +93,7 @@ export let mySkills = [
         id: Math.random().toString(36).substr(2, 9),
         img: 'icon-materialui-plain',
         title: 'Material UI',
-        stars: 4,
+        stars: 4.5,
         starFull: "icon-star-full",
         starHalf: "icon-star-half",
         starEmpty: 'icon-star-empty'
@@ -102,7 +102,7 @@ export let mySkills = [
         id: Math.random().toString(36).substr(2, 9),
         img: 'icon-gulp-plain',
         title: 'GULP',
-        stars: 3.5,
+        stars: 4,
         starFull: "icon-star-full",
         starHalf: "icon-star-half",
         starEmpty: 'icon-star-empty'
@@ -111,15 +111,6 @@ export let mySkills = [
         id: Math.random().toString(36).substr(2, 9),
         img: 'icon-git-original',
         title: 'GIT',
-        stars: 4,
-        starFull: "icon-star-full",
-        starHalf: "icon-star-half",
-        starEmpty: 'icon-star-empty'
-    },
-    {
-        id: Math.random().toString(36).substr(2, 9),
-        img: 'icon-github-original',
-        title: 'Github',
         stars: 4,
         starFull: "icon-star-full",
         starHalf: "icon-star-half",
