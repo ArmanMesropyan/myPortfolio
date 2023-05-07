d9edb5a118e32f553c5508f082598fd4f3f696bb
Arman <mesropyanarman4@gmail.com>
2023-05-07 13:37:15 +0400

    header transition

--- src/pages/header/index.jsx
+++ src/pages/header/index.jsx
@@ -1,30 +1,53 @@
-import React from 'react'
-import { NavLink} from 'react-router-dom'
-import './index.scss'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./index.scss";
 
-const Header = ({mouseOverEvent, mouseOutEvent}) => {
-    return (
-        <div className='G-container L-header G-flex G-justify-between G-flex-wrap'>
-            <div className='L-header-logo'>
-                <NavLink
-                    onMouseOver={mouseOverEvent}
-                    onMouseOut={mouseOutEvent}
-                    className='L-header--link' to='/'> Arman Mesropyan</NavLink>
-            </div>
-            <div>
-                <ul className='L-header-links G-flex'>
-                    <NavLink onMouseOver={mouseOverEvent}
-                          onMouseOut={mouseOutEvent}
-                          className='L-header--link' to='/Projects'>My Projects</NavLink>
-                    <NavLink onMouseOver={mouseOverEvent}
-                          onMouseOut={mouseOutEvent}
-                          className='L-header--link' to='/Skills'>Skills</NavLink>
-                    <NavLink onMouseOver={mouseOverEvent}
-                          onMouseOut={mouseOutEvent}
-                          className='L-header--link' to='/AboutMe'>About Me</NavLink>
-                </ul>
-            </div>
-        </div>
-    )
-}
-export default Header
+const Header = ({ mouseOverEvent, mouseOutEvent }) => {
+  return (
+    <div
+      className="G-container L-header G-flex G-justify-between G-flex-wrap "
+      data-aos="fade-down"
+    >
+      <div className="L-header-logo">
+        <NavLink
+          onMouseOver={mouseOverEvent}
+          onMouseOut={mouseOutEvent}
+          className="L-header--link"
+          to="/"
+        >
+          {" "}
+          Arman Mesropyan
+        </NavLink>
+      </div>
+      <div>
+        <ul className="L-header-links G-flex">
+          <NavLink
+            onMouseOver={mouseOverEvent}
+            onMouseOut={mouseOutEvent}
+            className="L-header--link"
+            to="/Projects"
+          >
+            My Projects
+          </NavLink>
+          <NavLink
+            onMouseOver={mouseOverEvent}
+            onMouseOut={mouseOutEvent}
+            className="L-header--link"
+            to="/Skills"
+          >
+            Skills
+          </NavLink>
+          <NavLink
+            onMouseOver={mouseOverEvent}
+            onMouseOut={mouseOutEvent}
+            className="L-header--link"
+            to="/AboutMe"
+          >
+            About Me
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
